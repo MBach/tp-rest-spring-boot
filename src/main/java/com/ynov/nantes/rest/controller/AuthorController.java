6c99d32981b6367bbf77100a5d74a2331f46c5d8
Matthieu BACHELIER <bachelier.matthieu@gmail.com>
2020-11-06 11:53:45 +0100

    Add delete

--- src/main/java/com/ynov/nantes/rest/controller/AuthorController.java
+++ src/main/java/com/ynov/nantes/rest/controller/AuthorController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,11 @@ public class AuthorController {
         } catch (Exception e) {
             return null;
         }
+    }
+    
+    @DeleteMapping("/author/{id}")
+    public void deleteAuthor(final @PathVariable("id") Integer authorId) {
+        authorRepository.deleteById(authorId);
     }
 
     @GetMapping("/author")
